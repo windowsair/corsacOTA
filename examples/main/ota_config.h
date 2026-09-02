4bed0c3a761adb729a59edb9adcef33af283045e
windowsair <dev@airkyi.com>
2026-09-02 19:37:47 +0800

    feat: Add support for esp-idf v6.1
    ESP-IDF v4.4 has reached end-of-life. Bump the minimum version for
    ESP32 and other targets to v5.0.0.
    
    This does not affect the ESP8266 build. Additionally, add multiple
    ESP-IDF versions as CI build targets.

--- examples/main/ota_config.h
+++ examples/main/ota_config.h
@@ -1,11 +1,15 @@
 #ifndef _OTA_CONFIG_H_
 #define _OTA_CONFIG_H_
 
-#define CONFIG_EXAMPLE_WIFI_SSID "OTA"
-#define CONFIG_EXAMPLE_WIFI_PASSWORD "12345678"
+#include "sdkconfig.h"
+#if (defined CONFIG_IDF_TARGET_ESP8266) && (CONFIG_IDF_TARGET_ESP8266 == 1)
+    #define CONFIG_EXAMPLE_WIFI_SSID "OTA"
+    #define CONFIG_EXAMPLE_WIFI_PASSWORD "12345678"
+#endif
 
 // Use the address "ota.local" to access the device
 #define CONFIG_MDNS_HOSTNAME "ota"
 #define CONFIG_MDNS_INSTANCE "corsacOTA mDNS"
 
 #endif
+
